278bf816a9c78e3c0a080ea8dbc3336a2396f958
Adam Chen Wei <adamchenwei@gmail.com>
2018-10-19 12:47:27 -0400

    chore(): cleanup

--- src/components/ShowRoom2.vue
+++ src/components/ShowRoom2.vue
@@ -4,27 +4,7 @@
 
 <script>
 
-import { createHOC } from 'vue-hoc';
-// import Abc from './Abc.vue';
-// import AbcInJsx from './AbcInJsx.jsx'
-import withCustomComponent,{ WithCustom } from './decorator/withCustomComponent.js';
-import Name from './Name.vue';
 import bbc from './bbc.vue';
-
-// const CustomAbc = {
-//     name: 'CustomAbc',
-//     render() {
-//       console.log('custom abc')
-//       // return (
-//       //   <h1>1</h1>
-//       // )
-//       return (
-//         <WithCustom>
-//           <Abc />
-//         </WithCustom>
-//       )
-//     }
-//   };
 
   export default {
     name: 'ShowRoom2',
